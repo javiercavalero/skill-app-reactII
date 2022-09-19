06cbc8531a2dfc48f468caf93971ad07f90be7e5
javier cavalero <javicavalero56@gmail.com>
2022-09-19 16:42:57 -0300

    onBlur event in formik and UX added

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -17,7 +17,7 @@ export const TaskForm = () => {
     const required = "*Required field"
     const validationSchema = Yup.object().shape({
 
-        title: Yup.string().min(6,"enter a minimum of 6 characters").required(required),
+        title: Yup.string().min(6, "enter a minimum of 6 characters").required(required),
         status: Yup.string().required(required),
         priority: Yup.string().required(required),
 
@@ -25,36 +25,51 @@ export const TaskForm = () => {
 
     const formik = useFormik({ initialValues, validationSchema, onSubmit });
 
-    const { handleSubmit, handleChange, errors } = formik
+    const { handleSubmit, handleChange, errors, touched, handleBlur } = formik
 
     return <section className="task-form">
         <h2>Create task</h2>
         <p>Create your tasks</p>
         <form onSubmit={handleSubmit}>
             <div>
-                <div> 
-                <input name="title" onChange={handleChange}  placeholder="title"/>
-                 {errors.title && <span>{errors.title}</span> }
-                </div>
-                
                 <div>
-                    <select name="status" onChange={handleChange} >
+                    <input
+                        name="title"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="title"
+                        className={errors.title ? "error" : ""}
+                    />
+                    {errors.title && touched.title && <span className="error-message">{errors.title}</span>}
+                </div>
+
+                <div>
+                    <select
+                        className={errors.status ? "error" : ""}
+                        name="status"
+                        onChange={handleChange}
+                        onBlur={handleBlur}>
+
                         <option value="">Select state</option>
                         <option value="new">New</option>
                         <option value="inProcess">In process</option>
                         <option value="finished">Finished</option>
                     </select>
-                    {errors.status && <span>{errors.status}</span> }
+                    {errors.status && touched.status && <span className="error-message">{errors.status}</span>}
 
                 </div>
                 <div>
-                 <select name="priority" onChange={handleChange} >
-                    <option value="">Select priority</option>
-                    <option value="low">Low</option>
-                    <option value="medium">Medium</option>
-                    <option value="high">High</option>
-                </select>
-                {errors.priority && <span>{errors.priority}</span> }
+                    <select
+                        name="priority"
+                        className={errors.priority ? "error" : ""}
+                        onChange={handleChange}
+                        onBlur={handleBlur}>
+                        <option value="">Select priority</option>
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                    {errors.priority && touched.priority && <span className="error-message">{errors.priority}</span>}
 
                 </div>
             </div>
